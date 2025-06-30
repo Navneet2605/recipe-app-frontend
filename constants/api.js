d3e78b89e486bbe84e72b9280b9040668ea14d90
Navneet2605 <navneetpcs3771@gmail.com>
2025-06-30 14:02:34 +0530

    staring the recipe screen

--- constants/api.js
+++ constants/api.js
@@ -1,3 +1,8 @@
 // if you're using your physical phone, change this to the deployed url
 // we have explained this in the course :-)
 export const API_URL = "http://localhost:5001/api";
+
+
+// while using the mobile instead of Simulator
+
+// export const API_URL = "https://recipe-app-backend-h8h2.onrender.com/api";
