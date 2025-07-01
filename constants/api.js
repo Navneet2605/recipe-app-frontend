5a6c436328db3d30947bbfd868457d503097d39b
Navneet2605 <navneetpcs3771@gmail.com>
2025-07-01 16:06:06 +0530

    API edit to work on phone

--- constants/api.js
+++ constants/api.js
@@ -1,8 +1,8 @@
 // if you're using your physical phone, change this to the deployed url
 // we have explained this in the course :-)
-export const API_URL = "http://localhost:5001/api";
+// export const API_URL = "http://localhost:5001/api";
 
 
 // while using the mobile instead of Simulator
 
-// export const API_URL = "https://recipe-app-backend-h8h2.onrender.com/api";
+export const API_URL = "https://recipe-app-backend-h8h2.onrender.com/api";
